687fddb30132631451c04f8322cfa63c8073dd94
Lakshitha <87607134+HYPER-MODZ@users.noreply.github.com>
2023-02-26 18:52:40 +0530

    Update config.ts

--- config.ts
+++ config.ts
@@ -30,7 +30,7 @@ const config: Config = {
         video: true, // much slower and works only with mode=AI_PAINTING_ANIME
     },
     parallelRequests: 10,
-    proxyUrl: 'socks5://:Elite:Elite@218.76.101.25:7302',
+    proxyUrl: 'socks5://:HIA:HIA@111.199.70.108:1080',
 };
 
 export default config;
