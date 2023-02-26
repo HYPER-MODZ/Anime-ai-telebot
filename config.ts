ba189490f02167ec4b8224858fb3841037fc32f0
S D <126170428+sl-coder84@users.noreply.github.com>
2023-02-26 11:01:48 +0000

    Add files via upload

---
+++ config.ts
@@ -0,0 +1,28 @@
+﻿const config: Config = {
+    botToken: '5778803513:AAGqTkyF--atxLBdKisq4xx6YXgFUvSiUW8',
+    keepFiles: {
+        compared: false,
+        input: false,
+        single: false,
+        video: false,
+    },
+    messages: {
+        blocked: 'The Chinese website has blocked the bot, too bad 🤷‍♂️',
+        bye:
+            '💚 Thanks For Using Bot 💚\n' +
+            '💚 You can add bot your groups 💚\n' +
+            '💚 [Bot Channel](https://t.me/animebotc) 💚\n' +
+            'special Thanks For script Owner Imcsu',
+        hello: 'Hi 👋 Send me a photo to convert it into a 2D anime art\n 🌸 Be sure to join the channel @animebotc',
+        media: '@Anime\\qq\\Ai\\_bot',
+       received: 'Photo has been received please wait 🍀',
+    },
+    proxyUrl: 'socks5://HIA:HIA@58.49.230.248:30001',
+    sendMedia: {
+        compared: true,
+        single: true,
+        video: true,
+    },
+};
+
+export default config;
